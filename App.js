84e0dde0d1c69fe9bcf9f44546d96688b243005c
StoneLabs <levyehrstein@googlemail.com>
2019-12-17 23:11:58 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Game from './src/Game';
+
+export default class App extends Component {
+  render() { return (<Game/>); } 
+}
---
+++ src/Game.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default class Game extends Component {
+  render() {
+      return (
+        <Text>Hello world!</Text>
+      );
+  }
+} 
